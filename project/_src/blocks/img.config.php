2ba155938c7b64855d55b2a98df1eebfb5bda019
Juliette Salomon <juliette.salomon@gmail.com>
2019-03-19 08:58:30 +0100

    block img gestion de 2 templates + zoom

--- project/_src/blocks/img.config.php
+++ project/_src/blocks/img.config.php
@@ -5,6 +5,26 @@
  *
  */
 ?>
+<label>Images</label>
+<?=$vv->wysiwyg()->field("images")
+        ->recordPicker("filerecord",true)
+    ->onSavedRefreshListItem($vv)
+        ->onlyFiles()
+        ->setMimeAcceptImagesOnly()
+        ->buttonRecord()
+        ->render()
+?>
+<label>Style</label>
+<?=$vv->wysiwyg()
+    ->field("style")
+    ->string()
+    ->onSavedRefreshListItem($vv)
+    ->select([
+            "Une image"=>"one-image",
+            "Deux images"=>"two-images",
+    ])
+?>
+
 <label>Activer le zoom?</label>
 <?=$vv->wysiwyg()
     ->field("zoom")
--- project/_src/blocks/img.php
+++ project/_src/blocks/img.php
@@ -1,39 +1,79 @@
 <?php
 /**
- *
  * @var Classiq\Models\JsonModels\ListItem $vv
- *
- *
  */
-/** @var bool $zoom */
-$zoom=$vv->getData("zoom");
-/** @var \Classiq\Models\Filerecord $img */
-$img=$vv->getDataAsRecord("img");
+use Classiq\Models\Filerecord;
 
-$imgZoom="";
-if($img && $img->isImage() && $zoom){
-    $imgZoom=$img->image()->sizeMax(1600,1600)->jpg()->href();
+
+/** @var Filerecord[] $imgs La liste d'image */
+$imgs=$vv->getDataAsRecords("images");
+
+/** @var Filerecord $image1 La première image */
+$image1=null;
+
+/** @var Filerecord $image2 La deuxième image */
+$image2=null;
+
+/** @var string $style une ou deux images ? */
+$style=$vv->getData("style","one-image");
+if($style==="two-images" && count($imgs)<2){
+    //si il n'y a qu'une seule image...
+    $style="one-image";
 }
+
+if($imgs){
+    $image1=array_shift($imgs);
+    if($style==="two-images"){
+        $image2=array_shift($imgs);
+    }
+}
+
+
+
 ?>
-<div <?=$vv->wysiwyg()->attr()?> class="block block-img py-medium">
+<div <?=$vv->wysiwyg()->openConfigOnCreate()->attr()?> class="block block-img py-medium">
     <div class="container">
-        <div class="cadre">
-            <div class="img-wrap"  <?if($imgZoom):?>data-zoom-img="<?=$imgZoom?>"<?endif;?>  >
-                <?=$vv->wysiwyg()
-                    ->field("img")
-                    ->image()
-                    ->format()
-                    ->width(1200)
-                    ->jpg()
-                    ->htmlTag()
-                    ->addClass("img-responsive")
-                ?>
-                <?if($zoom):?>
-                    <?=pov()->svg->use("startup-zoom")?>
-                <?endif?>
-            </div>
+        <div class="row cadre <?=$imgs?"multiple":""?>">
+
+            <?if($style==="two-images"):?>
+
+                <div class="col-6 img-wrap" data-zoom-img="<?=$image1->image()->sizeMax(1600,1600)->jpg()->href()?>"  >
+                    <?=$image1->image()->sizeCover(600,900)
+                        ->jpg()
+                        ->htmlTag()
+                        ->addClass("img-responsive")?>
+                </div>
+
+                <div class="col-6 img-wrap" data-zoom-img="<?=$image2->image()->sizeMax(1600,1600)->jpg()->href()?>"  >
+                    <?=$image2->image()->sizeCover(600,900)
+                        ->jpg()
+                        ->htmlTag()
+                        ->addClass("img-responsive")?>
+                </div>
+
+            <?else:?>
+
+                <?if($image1):?>
+                    <div class="col-12 img-wrap" data-zoom-img="<?=$image1->image()->sizeMax(1600,1600)->jpg()->href()?>"  >
+                        <?=$image1->image()->width(1200)
+                            ->jpg()
+                            ->htmlTag()
+                            ->addClass("img-responsive")?>
+                    </div>
+                <?endif;?>
+
+            <?endif?>
+
 
         </div>
     </div>
+
+    <?//balises invisibles pour les autres images de zoom?>
+    <div style="display: none;">
+        <?foreach ($imgs as $image):?>
+            <div data-zoom-img="<?=$image->image()->sizeMax(1600,1600)->jpg()->href()?>"></div>
+        <?endforeach;?>
+    </div>
+
 </div>
 
