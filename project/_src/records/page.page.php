456f18bd1110b9d5c8c60fdb32b815079e36207f
david <d.marsalone@gmail.com>
2019-04-16 10:21:05 +0200

    simplification code

--- project/_src/records/page.page.php
+++ project/_src/records/page.page.php
@@ -13,16 +13,9 @@ $view->inside("layout/layout", $vv);
             --page-color: <?=site()->getPageColor($vv)?>;
         }
     </style>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1074" height="804"
-         viewBox="0 0 1074 804">
-        <defs>
-            <linearGradient id="linear-gradient" x1="0.5" x2="0.5" y2="1" gradientUnits="objectBoundingBox">
-                <stop offset="0" stop-color="<?= site()->getPageColor($vv) ?>" stop-opacity="1"/>
-                <stop offset="1" stop-opacity="0"/>
-            </linearGradient>
-        </defs>
-        <path d="M-220,0H854V804H710Z" transform="translate(220)" fill="url(#linear-gradient)"/>
-    </svg>
+
+    <?=$view->render("components/angle-degrade",$vv)?>
+
 
     <div class="container page-header">
 
