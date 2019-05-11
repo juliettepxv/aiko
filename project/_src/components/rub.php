b9b32adffbf5e5a174b1e34eec5b7385a650c4d7
david <d.marsalone@gmail.com>
2019-05-11 08:21:37 +0200

    sprites rubs

--- project/_src/components/rub.php
+++ project/_src/components/rub.php
@@ -2,5 +2,20 @@
 /** @var \Classiq\Models\Page $vv */
 ?>
 <h1 <?=$view->attrRefresh($vv->uid())?> class="rub <?=$vv->getValue("vars.tache")?>">
-    <?=$vv->getValue("shortname_".the()->project->langCode)?>
+
+
+    <div class="sprites">
+        <div class="sprite-a animate filter-black2blue"></div>
+        <div class="sprite-a animate filter-black2purple"></div>
+        <div class="sprite-a animate filter-black2green"></div>
+        <div class="sprite-a animate filter-black2orange"></div>
+        <?/*
+        <div class="sprite-a animate filter-black2blue"></div>
+        <div class="sprite-a animate filter-black2orange"></div>
+        <div class="sprite-a animate filter-black2yellow"></div>
+        */?>
+    </div>
+    <span>
+        <?=$vv->getValue("shortname_".the()->project->langCode)?>
+    </span>
 </h1>
--- project/_src/quick-view/motion-test.php
+++ project/_src/quick-view/motion-test.php
@@ -5,21 +5,19 @@ $view->inside("layout/layout", $vv);
 <div class="container pt-big" style="min-height: 100vh;">
     <h1>YO</h1>
     <div class="sprites">
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
-        <div class="sprite-a animate"></div>
+        <div class="sprite-a animate filter-black2blue"></div>
+        <div class="sprite-a animate filter-black2purple"></div>
+        <div class="sprite-a animate filter-black2green"></div>
+        <?/*
+        <div class="sprite-a animate filter-black2blue"></div>
+        <div class="sprite-a animate filter-black2orange"></div>
+        <div class="sprite-a animate filter-black2yellow"></div>
+        */?>
     </div>
 
 </div>
 <script>
-    :/*
+    /*
     setTimeout(function(){
         let i;
         let clones=[];
