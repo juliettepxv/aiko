0338ead6074e5c8aa4ccef2b1cd8be4873dcb686
david <d.marsalone@gmail.com>
2019-07-01 08:01:25 +0200

    configs de prod

---
+++ configs/routes/shared-prod.php
@@ -0,0 +1,24 @@
+<?php
+the()->project->languages=["fr","en"];
+the()->project->languagesUrls=[
+"fr"=>"http://aiko-creative.fr",
+"en"=>"http://aiko-creative.com"
+];
+//force https?
+the()->configProjectUrl->forceHttps=false;
+// Traductions ui.
+// https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8
+// Copiez-collez ce google Sheet pour pouvoir le modifier puis modifiez l'url. Vous pouvez bien entendu utiliser un csv en local mais c'est moins pratique :)
+the()->project->config_translations_csv_url="https://docs.google.com/spreadsheets/d/1kefhXt0Z7g0P2cGItQK4k_hqhSoFTMaJyvzNYjKeqw8/export?gid=0&format=csv";
+the()->project->config_translations_debug=true; //quand true recharge à chaque fois le CSV
+
+//config options
+
+//Google Map etc...
+//site()->googleApiKey="xxxxx";
+
+//Google webmaster tools
+//site()->googleSiteVerification="xxxxx";
+
+//Google analytics
+//site()->googleAnalyticsId="UA-xxxxxx";
